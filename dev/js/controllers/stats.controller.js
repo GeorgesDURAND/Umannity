f0f544a463953bdb9070a9011a2cb3b076d91f82
Jérémie A <mail@lp1.eu>
2016-10-14 22:43:59 +0200

    added new stats

--- dev/js/controllers/stats.controller.js
+++ dev/js/controllers/stats.controller.js
@@ -10,7 +10,7 @@
     function statsController($scope, UserService, RestService) {
         /* jshint validthis: true */
         var vm = this;
-        var _stats_routes = ["/users"];
+        var _stats_routes = ["/users", "/requests", "/offers"];
 
         vm.name = "statsController";
 
@@ -20,24 +20,28 @@
 
         function onViewContentLoaded(){
             vm.user = UserService.getUser();
-            console.log("statsController :: content loaded");
+            console.log(vm.name + " :: content loaded");
             if (undefined === vm.charts) {
-                console.log("statsController :: no charts loaded, downloading charts...");
+                console.log(vm.name + " :: no charts loaded, downloading charts...");
                 getCharts();
             }
         }
 
         function parseData(data) {
+            console.log(vm.name + " :: parsing data", data);
             if (typeof undefined !== data.data.charts) {
                 if (undefined === vm.charts) {
                     vm.charts = [];
                 }
-                vm.charts = angular.extend(vm.charts, data.data.charts);
+                angular.forEach(data.data.charts, function(chart){
+                    vm.charts.push(chart);
+                });
             }
         }
 
         function getCharts() {
             angular.forEach(_stats_routes, function (route) {
+                console.log(vm.name + " :: calling route", route);
                 RestService.get("/stats" + route)
                     .then(function (data) {
                         parseData(data);
