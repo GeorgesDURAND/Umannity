c231ecb5cb2956b7f1813f45b9a7895ae8a07739
Lupin <lupin@lp1.eu>
2016-11-03 12:58:56 +0100

    fix on profile.controller

--- dev/js/controllers/profile.controller.js
+++ dev/js/controllers/profile.controller.js
@@ -1,32 +1,33 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('umannityApp.controllers')
-        .controller('profileController', profileController);
+  angular
+    .module('umannityApp.controllers')
+    .controller('profileController', profileController);
 
-    profileController.$inject = ['$scope', 'UserService'];
+  profileController.$inject = [ '$scope', 'UserService' ];
 
-    function profileController($scope, UserService) {
-        var vm = this;
-        $scope.$on('$viewContentLoaded', onViewContentLoaded);
+  function profileController ($scope, UserService) {
+    var vm = this;
+    $scope.$on('$viewContentLoaded', onViewContentLoaded);
 
-        function onViewContentLoaded() {
-            vm.user = UserService.getUser();
-            vm.picture = UserService.getPicture();
-            if (undefined === vm.picture) {
-                UserService.loadPicture()
-                    .then(function (picture) {
-                        vm.picture = picture;
-                    })
-                    .catch(function (error){
-                        console.log(error)
-                    });
-            }
-
-            console.log(vm.name + " :: content loaded");
-            console.log(vm.user);
-            console.log(vm.picture);
-        }
+    function onViewContentLoaded () {
+      //Loading the user
+      vm.user = UserService.getUser();
+      if (undefined === vm.user) {
+        UserService.loadUser()
+          .then(function (user) {
+            vm.user = user;
+          });
+      }
+      //Loading the picture
+      vm.picture = UserService.getPicture();
+      if (undefined === vm.picture) {
+        UserService.loadPicture()
+          .then(function (picture) {
+            vm.picture = picture;
+          });
+      }
     }
+  }
 })();
