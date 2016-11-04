e42aac94d251284d73949c4271e32bfb89588afc
root <root@umannity.com>
2016-11-04 13:28:13 +0100

    updated constant service functionning

--- dev/js/services/constant.service.js
+++ dev/js/services/constant.service.js
@@ -7,13 +7,17 @@
 
     function constantService() {
         var service = {
-            api_protocol : 'http',
-            api_host : 'localhost',
-            api_port : '1338'
+            api_protocol : 'https',
+            api_host : 'umannity.com/api',
+            api_port : ''
         };
 
-        service.api_url = service.api_protocol + '://' + service.api_host + ':' + service.api_port;
-
+	if (service.api_port.length > 0) {
+            service.api_url = service.api_protocol + '://' + service.api_host + ':' + service.api_port;
+	}
+	else {
+	    service.api_url = service.api_protocol + '://' + service.api_host;
+	}
         return service;
 
         ////
