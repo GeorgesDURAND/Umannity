259bcd1694484584e898e04e206575030cd4d91d
Alvaro <alvaro.idjoubar@epitech.eu>
2016-11-04 16:17:47 +0100

    edition du profile

--- dev/js/controllers/profile.controller.js
+++ dev/js/controllers/profile.controller.js
@@ -142,4 +142,31 @@
                 })
         }
     }
+    function onViewContentLoaded () {
+      //Loading the user
+      vm.user = UserService.getUser();
+      if (undefined === vm.user) {
+        UserService.loadUser()
+          .then(function (user) {
+            vm.user = user;
+          });
+      }
+      //Loading the picture
+      vm.picture = UserService.getPicture();
+      if (undefined === vm.picture) {
+        UserService.loadPicture()
+          .then(function (picture) {
+            vm.picture = picture;
+          });
+      }
+    }
+
+    function editProfile () {
+      console.log("editdProfileController");
+      if (undefined != vm.edited_user) {
+        console.log("not undefinned");
+        UserService.editProfile(vm.edited_user)
+      }
+    }
+  }
 })();
