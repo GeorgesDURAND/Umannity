6e5d2b92dfdf472c49e01b9ba220a330dfca861a
PC-Jeanne\Umannity <jeanne.lespinasse@epitech.eu>
2016-12-03 17:45:31 +0100

    Ajout d'un filtre de recherche + traduction du placeholder

--- dev/js/controllers/reportsListAdmin.controller.js
+++ dev/js/controllers/reportsListAdmin.controller.js
@@ -12,6 +12,11 @@
         var vm = this;
         vm.name = "reportsListAdminController";
         vm.reportsList = [];
+        vm.filterQuery = "";
+        vm.isFiltered = false;
+
+        vm.unReport = unReport;
+        vm.deleteRequest = deleteRequest;
 
         $scope.$on('$viewContentLoaded', onViewContentLoaded);
 
@@ -23,26 +28,47 @@
             RestService.get("/report")
                 .then(function (reports) {
                 vm.reportsList = reports.data;
-
-                /* Get the name of the user who reported the request */
                 angular.forEach(vm.reportsList, function(report, key){
                     UserService.loadUser(report.user_id)
                         .then(function (user) {
                         vm.reportsList[key].user_name = user.first_name + " " + user.last_name;
                     });
+                    vm.reportsList[key].date = UserService.formatBirthdate(vm.reportsList[key].date);
                 })
-
-                /* Get the name of the repoted request */
-                var data = {id:vm.reportsList.request_id};
-                console.log(vm.reportsList);
-                RestService.get("/request", data)
-                    .then(function (request) {
-                    console.log("REQUEST :: ", request);
-                })
-
-                console.log(vm.reportsList);
             })
         }
 
+        function deleteRequest(request_id) {
+            RestService.delete("/request", {request_id:request_id})
+                .then (function (data) {
+                if (data.status === 200){
+                    console.log("Request :: correctly deleted");
+                    vm.unReport(request_id);
+                }else {
+                    console.log("Request :: Error while tempting to delete");   
+                }
+            })
+        }
+
+        function unReport(request_id) {
+            RestService.delete("/report", {request_id:request_id})
+                .then (function (data) {
+                if (data.status === 200){
+                    console.log("Report :: correctly deleted");
+                    _refreshReportList(request_id);
+                }else {
+                    console.log("Report :: Error while tempting to delete");   
+                }
+            })
+        }
+
+        function _refreshReportList(request_id) {
+            angular.forEach(vm.reportsList, function(report, key){
+                if(report.request_id === request_id){
+                    vm.reportsList.splice(key, 1);
+                    console.log("ReportsList :: correctly refreshed");
+                }
+            })
+        }
     }
 })();
