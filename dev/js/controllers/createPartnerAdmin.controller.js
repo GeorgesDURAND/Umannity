8887b69a30577c0c430658f7aae68e89107b8ec2
PC-Jeanne\Umannity <jeanne.lespinasse@epitech.eu>
2016-12-03 19:56:50 +0100

    La création avance, sauvegarde avant la gestion d'image

--- dev/js/controllers/createPartnerAdmin.controller.js
+++ dev/js/controllers/createPartnerAdmin.controller.js
@@ -5,12 +5,43 @@
         .module('umannityApp.controllers')
         .controller('createPartnerAdminController', createPartnerAdminController);
 
-    createPartnerAdminController.$inject = ['$scope', 'UserService', 'RestService', '$translate'];
+    createPartnerAdminController.$inject = ['$scope', 'UserService', 'RestService', 'ngImgCrop'];
 
     function createPartnerAdminController($scope, UserService, RestService, $translate) {
         /* jshint validthis: true */
         var vm = this;
+        vm.newPartner = {};
+        vm.newPartner.max_offers = 1;
+        vm.partnerLogo = '';
+        vm.partnerCropLogo = '';
+        
+        vm.createNewPartner = createNewPartner;
 
         vm.name = "createPartnerAdminController";
+        
+        function createNewPartner() {
+            console.log(vm.newPartner);
+        }
+
+        var handleFileSelect = function(evt) {
+            var file = evt.currentTarget.files[0];
+            var reader = new FileReader();
+            reader.onload = function (evt) {
+                $scope.$apply(function($scope){
+                    $scope.myImage=evt.target.result;
+                });
+            };
+            reader.readAsDataURL(file);
+        };
+        angular.element(document.querySelector('#fileInput')).on('chan
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 })();
