0136bcff86e85359d425b62f57b6dff2d4517e95
PC-Jeanne\Umannity <jeanne.lespinasse@epitech.eu>
2016-12-03 17:49:22 +0100

    Norme OK

--- dev/js/controllers/reportsListAdmin.controller.js
+++ dev/js/controllers/reportsListAdmin.controller.js
@@ -8,7 +8,7 @@
     reportsListAdminController.$inject = ['$scope', 'UserService', 'RestService'];
 
     function reportsListAdminController($scope, UserService, RestService) {
-
+        /* jshint validthis: true */
         var vm = this;
         vm.name = "reportsListAdminController";
         vm.reportsList = [];
@@ -28,47 +28,47 @@
             RestService.get("/report")
                 .then(function (reports) {
                 vm.reportsList = reports.data;
-                angular.forEach(vm.reportsList, function(report, key){
+                angular.forEach(vm.reportsList, function(report, key) {
                     UserService.loadUser(report.user_id)
                         .then(function (user) {
                         vm.reportsList[key].user_name = user.first_name + " " + user.last_name;
                     });
                     vm.reportsList[key].date = UserService.formatBirthdate(vm.reportsList[key].date);
-                })
-            })
+                });
+            });
         }
 
         function deleteRequest(request_id) {
             RestService.delete("/request", {request_id:request_id})
                 .then (function (data) {
-                if (data.status === 200){
+                if (data.status === 200) {
                     console.log("Request :: correctly deleted");
                     vm.unReport(request_id);
                 }else {
                     console.log("Request :: Error while tempting to delete");   
                 }
-            })
+            });
         }
 
         function unReport(request_id) {
             RestService.delete("/report", {request_id:request_id})
                 .then (function (data) {
-                if (data.status === 200){
+                if (data.status === 200) {
                     console.log("Report :: correctly deleted");
                     _refreshReportList(request_id);
                 }else {
                     console.log("Report :: Error while tempting to delete");   
                 }
-            })
+            });
         }
 
         function _refreshReportList(request_id) {
-            angular.forEach(vm.reportsList, function(report, key){
-                if(report.request_id === request_id){
+            angular.forEach(vm.reportsList, function(report, key) {
+                if (report.request_id === request_id) {
                     vm.reportsList.splice(key, 1);
                     console.log("ReportsList :: correctly refreshed");
                 }
-            })
+            });
         }
     }
 })();
