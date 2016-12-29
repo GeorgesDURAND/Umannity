0b0b9f87c19176a69c6c0b003646e4aa80658deb
Georges DURAND <georges.durand@epitech.eu>
2016-12-29 14:55:42 +0100

    Besoin de cacher / afficher + ng-value sur location et description

--- dev/js/umannityApp.routes.js
+++ dev/js/umannityApp.routes.js
@@ -64,6 +64,11 @@
             controller: 'requestsListController',
             controllerAs: 'vm'
         })
+            .when('/request/:requestId', {
+            templateUrl: "partials/request.html",
+            controller: 'requestController',
+            controllerAs: 'vm'
+        })
             .when('/admin/stats', {
             templateUrl: "partials/statsAdmin.html",
             controller: 'statsAdminController',
